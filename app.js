3b36cfaeed8839d0e512e30e3469efdfe7785d1f
safouene mani <safouenemani@gmail.com>
2022-11-09 08:34:02 +0100

    adding form and submit post function to movies.ejs

--- app.js
+++ app.js
@@ -1,27 +1,54 @@
 const express = require('express');
 const app = express();
-
+const bodyParser =require('body-parser')
 const PORT = 3000;
+let frenchMovies =[];
 
-app.set('views','C:\Users\safouene.mani\Desktop\projects\GPMILL-landing-zone\src\views');
-app.set('view engine');
+app.use('/public', express.static('public'));
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('views', './views');
+app.set('view engine','ejs');
 
 app.get('/movies',(req,res) => {
-    res.send('bientot movies page');
+    const title = 'Films Français des 30 dernières annèes'
+    frenchMovies = [
+        {title: 'Le fabuleux destin de Safouene', year: 2001},
+        {title: 'Forrest Gump', year: 1998},
+        {title: 'Terminator', year: 2015},
+        {title: 'Avatar', year: 2022}
+    ];
+    res.render('movies', { movies: frenchMovies, title: title});
 });
 
-app.get('/movies/add',(req,res) => {
+//POST :
+var urlencodedParser = bodyParser.urlencoded({ extended: false});
+app.post('/movies', urlencodedParser,(req, res) => {
+    console.log('Le titre : ',req.body.movietitle);
+    console.log("L'annèe:",req.body.movieyear);
+    const newMovie = { title : req.body.movietitle, year :req.body.movieyear};
+    // frenchMovies.push(newmovie);  $[ la nouvelle version est ci-dessous]
+    frenchMovies = [...frenchMovies, newMovie];
+    console.log(frenchMovies);
+
+    res.sendStatus(201);
+});
+
+
+app.get('/movies/add', (req,res) => {
     res.send(`ajouter un film par içi`);
 });
 
-app.get('/movies/:id',(req,res) => {
+app.get('/movies/:id/:title',(req,res) => {
     const id = req.params.id;
-    res.send(`file numéro ${id}`);
+    const title = req.params.title;
+    res.render(`movies-details`, { movieid: id, movietitle: title});
 });
 
 
 app.get('/',(req,res) => {
-    res.send('Hello World !!');
+    // res.send('Hello World !!');
+    res.render('index')
 });
 
 app.listen(3000,() => {
